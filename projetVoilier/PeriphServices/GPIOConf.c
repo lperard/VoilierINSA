f9ffd5b644b97fb368ef95db9b5931e3e43757f8
Clement Gehin <cgehin@insat.insa-toulouse.fr>
2019-11-18 09:54:02 +0100

    merging issue

--- projetVoilier/PeriphServices/GPIOConf.c
+++ projetVoilier/PeriphServices/GPIOConf.c
@@ -5,9 +5,7 @@ void GPIOConfig(void) {
 
 	//enable gpio clock
 	RCC -> APB2ENR |= RCC_APB2ENR_IOPAEN;
-<<<<<<< HEAD
 	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
-=======
 	
 	//gpio conf for pwm servo moteur
 	LL_GPIO_InitTypeDef initGPIOA8;
@@ -16,6 +14,5 @@ void GPIOConfig(void) {
 	initGPIOA8.Mode = LL_GPIO_MODE_ALTERNATE;
 	initGPIOA8.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
 	LL_GPIO_Init(GPIOA, &initGPIOA8);
->>>>>>> 78e74a3762dd36dd44f964cd5b645501f03464b8
 	
 }

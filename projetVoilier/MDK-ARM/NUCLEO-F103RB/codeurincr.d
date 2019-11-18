4d84f712bd8a50a59581d20fc76e7b761b2196ba
Ruben Gres <rgres@etud.insa-toulouse.fr>
2019-11-18 10:00:23 +0100

    merging issue #20165

--- projetVoilier/MDK-ARM/NUCLEO-F103RB/codeurincr.d
+++ projetVoilier/MDK-ARM/NUCLEO-F103RB/codeurincr.d
@@ -3,10 +3,10 @@ nucleo-f103rb\codeurincr.o: ..\MyDrivers\codeurIncr.h
 nucleo-f103rb\codeurincr.o: ..\LLDrivers\inc\stm32f1xx_ll_tim.h
 nucleo-f103rb\codeurincr.o: ..\Inc\stm32f1xx.h
 nucleo-f103rb\codeurincr.o: ..\Inc\stm32f103xb.h
-nucleo-f103rb\codeurincr.o: C:\Users\Lucas Perard\AppData\Local\Arm\Packs\ARM\CMSIS\5.5.1\CMSIS\Core\Include\core_cm3.h
+nucleo-f103rb\codeurincr.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.3.0\CMSIS\Include\core_cm3.h
 nucleo-f103rb\codeurincr.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-nucleo-f103rb\codeurincr.o: C:\Users\Lucas Perard\AppData\Local\Arm\Packs\ARM\CMSIS\5.5.1\CMSIS\Core\Include\cmsis_version.h
-nucleo-f103rb\codeurincr.o: C:\Users\Lucas Perard\AppData\Local\Arm\Packs\ARM\CMSIS\5.5.1\CMSIS\Core\Include\cmsis_compiler.h
-nucleo-f103rb\codeurincr.o: C:\Users\Lucas Perard\AppData\Local\Arm\Packs\ARM\CMSIS\5.5.1\CMSIS\Core\Include\cmsis_armcc.h
+nucleo-f103rb\codeurincr.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.3.0\CMSIS\Include\cmsis_version.h
+nucleo-f103rb\codeurincr.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.3.0\CMSIS\Include\cmsis_compiler.h
+nucleo-f103rb\codeurincr.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.3.0\CMSIS\Include\cmsis_armcc.h
 nucleo-f103rb\codeurincr.o: ..\Inc\system_stm32f1xx.h
 nucleo-f103rb\codeurincr.o: ..\LLDrivers\inc\stm32f1xx_ll_gpio.h
--- projetVoilier/PeriphServices/GPIOConf.h
+++ projetVoilier/PeriphServices/GPIOConf.h
@@ -6,7 +6,3 @@
 void GPIOConfig(void);
 
 #endif
-<<<<<<< HEAD
-=======
-
->>>>>>> 78e74a3762dd36dd44f964cd5b645501f03464b8
